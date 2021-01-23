2ef65c39b635cdb2bd874348d55933c69f7ac25c
段会会 <1533289566@qq.com>
2021-01-23 23:39:57 +0800

    书籍展示与信息预览

--- src/components/2Resource/pages/book.tsx
+++ src/components/2Resource/pages/book.tsx
@@ -1,9 +1,14 @@
 import React,{Component} from 'react'
-import { Row,Col,Divider} from 'antd';
+import { Row,Col,Divider,Image,Typography, Space} from 'antd';
+import ImageJ from "images/j.png";
+import './pages.less'
+
+const { Text } = Typography;
 
 export default class Book extends Component{
     state={
-        bookList:[{}]
+        bookList:[{}],
+        isShowBookDetail:false
     }
     onLoadBook=()=>{
         const {bookList} = this.state
@@ -14,13 +19,52 @@ export default class Book extends Component{
             bookList:bookList
         })
     }
+    //鼠标进入书籍封面展示详细信息 onMouseEnter / 
+    showBookDetail=()=>{
+        this.setState({
+            isShowBookDetail:true
+        } )
+        
+    }
+    //鼠标进入书籍封面隐藏详细信息 onMouseLeave
+    hideBookDeatil=()=>{
+        this.setState({
+            isShowBookDetail:false
+        } )
+        
+    }
 
     render(){
         return <>
         
         <Divider orientation="left">书籍</Divider>
-              <Row> 
-               
+              <Row gutter={[16, 16]}> 
+               <Col span={3} className='bookImage'onMouseEnter={this.showBookDetail} onMouseLeave={this.hideBookDeatil}>
+                   <Image src={ImageJ} ></Image>
+                   {this.state.isShowBookDetail&&
+                   <div className='bookDetail'>
+                       <Space direction="vertical" size={[0, 0]}>
+                            <Text strong> 三重门 </Text>
+                            <Text> 韩寒 </Text>
+                            <Text> 月黑风高三重门，唯我独存是谁争是非 </Text>
+                       </Space></div>
+                    }
+               </Col>
+               <Col span={3}><Image src={ImageJ}></Image></Col>
+               <Col span={3}><Image src={ImageJ}></Image></Col>
+               <Col span={3}><Image src={ImageJ}></Image></Col>
+               <Col span={3}><Image src={ImageJ}></Image></Col>
+               <Col span={3}><Image src={ImageJ}></Image></Col>
+               <Col span={3}><Image src={ImageJ}></Image></Col>
+               <Col span={3}><Image src={ImageJ}></Image></Col>
+               <Col span={3}><Image src={ImageJ}></Image></Col>
+               <Col span={3}><Image src={ImageJ}></Image></Col>
+               <Col span={3}><Image src={ImageJ}></Image></Col>
+               <Col span={3}><Image src={ImageJ}></Image></Col>
+               <Col span={3}><Image src={ImageJ}></Image></Col>
+               <Col span={3}><Image src={ImageJ}></Image></Col>
+               <Col span={3}><Image src={ImageJ}></Image></Col>
+               <Col span={3}><Image src={ImageJ}></Image></Col>
               </Row>
         <Divider orientation="left">国家等级考试</Divider>
                 <Row> 
