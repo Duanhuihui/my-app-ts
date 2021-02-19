18d60effecd6592b46066ded9032e123fe4c95bb
段会会 <1533289566@qq.com>
2021-02-20 00:00:48 +0800

    优化题目排列，新增判断题类型

--- src/components/5Exercises/pages/exerciseList.tsx
+++ src/components/5Exercises/pages/exerciseList.tsx
@@ -1,20 +1,17 @@
 import React from 'react'
-import {Row,Col,Radio,Checkbox} from 'antd'
+import {Row,Col,Radio,Checkbox,Tag} from 'antd'
 
 export default class ExerciseList extends React.Component{
    
     render(){
         const radioStyle = { display: 'block', height: '30px', lineHeight: '30px', };
+        const judge = { display: 'block', height: '30px', lineHeight: '30px', };
         return <>
             <Row gutter={5} style={{paddingTop:'10px' }}>
-                <Col span={1}>NO.1</Col>
-                <Col span={3}>单项选择题</Col>
+                <Col span={24}><Tag color="#f50">单选</Tag>1.这是题目描述，请根据题目描述选择正确的答案。</Col>
             </Row>
             <div style={{paddingLeft:'20px'}}>
             <Row gutter={5} style={{paddingTop:'10px' }}>
-                <Col>这是题目描述，请根据题目描述选择正确的答案。</Col>
-            </Row>
-            <Row gutter={5}>
                 <Col>
                 <Radio.Group >
                 <Radio style={radioStyle} value={1}>
@@ -34,14 +31,10 @@ export default class ExerciseList extends React.Component{
             </Row>
             </div>
             <Row gutter={5} style={{paddingTop:'10px' }}>
-                <Col span={1}>NO.2</Col>
-                <Col span={3}>多项选择题</Col>
+            <Col span={24}><Tag color="#f50">多选</Tag>2.这是题目描述，请根据题目描述选择正确的答案。</Col>
             </Row>
             <div style={{paddingLeft:'20px'}}>
             <Row gutter={5} style={{paddingTop:'10px' }}>
-                <Col>这是题目描述，请根据题目描述选择正确的答案。</Col>
-            </Row>
-            <Row gutter={5}>
                 <Col>
                 <Checkbox.Group style={{ width: '100%' }} >
                     <Row>
@@ -62,6 +55,24 @@ export default class ExerciseList extends React.Component{
                     </Col>
                     </Row>
                 </Checkbox.Group>
+                </Col>
+            </Row>
+            </div>
+            <Row gutter={5} style={{paddingTop:'10px' }}>
+                <Col span={24}><Tag color="#f50">判断</Tag>3.这是题目描述，请根据题目描述选择正确的答案。</Col>
+            </Row>
+            <div style={{paddingLeft:'20px'}}>
+            <Row  style={{paddingTop:'10px' }}>
+                <Col span={1}>
+                <Radio.Group >
+                <Radio value={1}>
+                </Radio>
+                <Radio  value={2} style={{color:'red'}}>
+                </Radio>
+                </Radio.Group>
+                </Col>
+                <Col span={23}>
+                判断题选项,判断题选项判断题选项判断题选项判断题选项判断题选项,判断题选项判断题选项判断题选项
                 </Col>
             </Row>
             </div>
