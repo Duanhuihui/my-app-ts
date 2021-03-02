d99959e0782b17f5f9cf8a1932aff82e74273abb
段会会 <1533289566@qq.com>
2021-03-02 23:55:01 +0800

    优化签到系统

--- src/components/6Communication/pages/signIn.tsx
+++ src/components/6Communication/pages/signIn.tsx
@@ -1,19 +1,33 @@
 import React from 'react'
-import { Calendar,Row,Col } from 'antd';
+import { Calendar,Row,Col,Divider,List,Button,Skeleton  } from 'antd';
 import {Map, Marker} from 'react-bmapgl';
 import moment from 'moment';
 import './pages.less'
 
+const data = [
+    'Racing ',
+    'Japanese ',
+    'Australian ',
+    'Man charged ',
+    'Los Angeles',
+  ];
 export default class SignIn extends React.Component<any,any>{
 
-    dateCellRender=(date:any)=> {
-        return(<div>111</div>)
-      }
+
     render(){
         return<>
         <Row gutter={[16, 32]}>
-            <Col span={18}>1</Col>
-            <Col span={6}>
+            <Col span={17}><Divider>课堂签到排行榜</Divider>
+            <Row><Col span={20} offset={2}><List
+                  itemLayout='vertical'
+                  dataSource={data}
+                  renderItem={item => <List.Item>{item}<div>content</div>
+                  </List.Item>}></List>
+                  <div style={{marginTop:'27px'}}><Button type="primary" block>签到</Button></div>
+                  </Col>
+            <Col span={2}></Col></Row>
+            </Col>
+            <Col span={7}>
             <div className="site-calendar-demo-card">
                 <Calendar fullscreen={false} />
             </div>
